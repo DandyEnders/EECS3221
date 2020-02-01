fc7d2f1f0d45e87483f7e97fd6a1c740fb3db0b5
Jinho Hwang <howden2@qraftec.com>
2020-01-31 21:04:50 -0500

    removed exit

--- miniproject1/miniproject1/main.c
+++ miniproject1/miniproject1/main.c
@@ -3,8 +3,11 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #define MAX_LINE 80 /* The maximum length command */
 #define HALF_LINE MAX_LINE/2 + 1
+#define N_CMD_TOKENS 4
+#define CMD_LEN 20
 
 /*
 
@@ -12,6 +15,7 @@
     Project description : https://github.com/hamzehkhazaei/EECS3221_MiniProj1/blob/master/Project_Description_and_Grading.md
 
     Objective:
+    0. Make makefile
     1. Creating the child process and executing the command in the child
     2. Providing a history feature
     3. Adding support of input and output redirection
@@ -19,22 +23,43 @@
 
 */
 
-//void my_prune_string(char* string){
-//    int i;
-//    int j;
-//    int cur_len = strlen(string);
-//    if(cur_len > 0){
-//        for(i = 0; i < cur_len; i++){
-//            if(string[i] < '!'){
-//                for(j = i; j < cur_len - 1; j++){
-//                    string[j] = string[j+1];
-//                }
-//                string[cur_len - 1] = '\0';
-//            }
-//        }
-//    }
-//
-//}
+char **my_tokenizer(char *a_input, char* separator){
+    int i = 0;
+
+    // make copy of a_input. Don't touch argument.
+    char *input = malloc(sizeof(a_input));
+    strcpy(input, a_input);
+
+    // argument tokens will have N_CMD_TOKENS many tokens.
+    char **arg_tokens = malloc (N_CMD_TOKENS * sizeof(char *));
+    // for(i = 0; i < N_CMD_TOKENS; i++){
+    //     arg_tokens[i] = malloc(CMD_LEN * sizeof(char *));
+    // }
+
+    // Check if malloc failed
+    if (arg_tokens == NULL){
+        fprintf(stderr, "malloc failed in function get_args_tokens.\n");
+    }
+
+    char *word;         // token word (temp)
+    i = 0;              // index to put in arg_tokens
+
+    // Tokenize input
+    word = strtok(input, separator);
+    while(word != NULL){
+        arg_tokens[i] = word;
+        word = strtok(NULL, separator);
+        i = i + 1;
+    }
+    arg_tokens[i] = NULL;
+    return arg_tokens;
+}
+
+char **get_args_tokens(char *input){
+    char *sep = " \n";  // separator for tokens
+    return my_tokenizer(input, sep);
+}
+
 
 int my_strcmp(char* string1, char* string2){
     if(strcmp(string1, string2) == 0){
@@ -45,76 +70,124 @@ int my_strcmp(char* string1, char* string2){
 }
 
 int main(void) {
-//    char *args[HALF_LINE]; /* command line arguments */
-    char args[MAX_LINE] = "\0";
-    char prev_args[MAX_LINE] = "\0"; // previous args;
-    char *args_token[HALF_LINE];
-    char *commands[3];
-    char *storage;
+    // Declaration
+    char*   args;           // current args
+    char*   prev_args;      // previous args
+    char**  args_token;     // tokenized args, read only
+    char**  commands;       // tokenized args, mutable
 
-    char *last_arg;
+    char    curr_dir[2048]; // pwd
 
-    int should_run = 1; /* flag to determine when to exit program */
+    int     is_background_task; // is next task background?
+    int     i;                  // variant    
 
-    fflush(stdout);
+    // Init
+    prev_args = malloc (MAX_LINE * sizeof(char *));
+    //args = malloc (MAX_LINE * sizeof(char *)); args is variable. capped at MAX_LINE
 
-    while (should_run){
-        printf("mysh:~$ ");
+    // Loop
+    while (1){
+
+        /*
+            Start init
+        */
+        // get curr dir right now.
+        getcwd(curr_dir, sizeof(curr_dir));
+        is_background_task = 0;
+
+        // Jinho shell, jsh
+        printf("jsh:%s$ ", curr_dir);
         fflush(stdout);
 
-        fgets(args, MAX_LINE, stdin);
-        if(args[strlen(args)-1] == '\n'){
-            args[strlen(args)-1] = '\0';
-        }
-//        printf("input: %s", args);
+        // fresh malloc
+        args = malloc (MAX_LINE * sizeof(char *));
 
-        // if args is something weird
-        char dummy_arg[MAX_LINE] = "\0";
-        strcpy(dummy_arg, args);
-        if(strtok(dummy_arg, " \n") == NULL){
-//            printf("NULL: %s", args);
+        /*
+            User Input
+        */
+        // read a line of input, let it be args
+        fgets(args, MAX_LINE, stdin);
+
+        // tokenize the input args
+        args_token = get_args_tokens(args);
+        
+        /*
+            Input processing
+        */
+        // if there is nothing, just go back.
+        if (!args_token[0]){
+            free(args);
+            free(args_token);
             continue;
         }
 
-//        printf("%s", args);
-        if(my_strcmp(args, "!!")){
-            if(my_strcmp(prev_args, "\0")){
-                printf("No commands in history.\n");
+        // execute history = !!
+        if (my_strcmp(args_token[0], "!!")){
+            if (!prev_args){ // when prev_args does not exist,
+                printf("No commands in history.");
                 continue;
-            }else{
-//              args = prev_args;
+            }else{ // When prev_args exists,
+                // args = prev_args
                 strcpy(args, prev_args);
+                args_token = get_args_tokens(args);
+                // and go on with execution
             }
+            
         }else{
-//          success mode so put prev_args now
-//          prev_args = args;
+            // When !! was not fired,
+            // prev_args will be args now.
             strcpy(prev_args, args);
         }
-        // Tokenize args
-        int i = 0;
-        storage = strtok(args, " \n");
-        while(storage != NULL){
-            args_token[i] = storage;
-//            printf("storage: %s", storage);
-            storage = strtok(NULL, " \n");
-            i = i + 1;
+
+        // Execute exit, pwd, cd
+        if(my_strcmp(args_token[0], "exit")){
+            printf("Bye!\n");
+            return(0);
         }
 
+        /* 
+            Mutable commands creation
+        */
+        //commands is mutable args_token.
+        commands = get_args_tokens(args);
 
-        // if last token is &
-        int is_background_process = my_strcmp(args_token[i-1], "&");
-        if(is_background_process){
-            args_token[i-1] = NULL;
+        // Check if background task.
+        // Start from last commands.
+        // We don't check the first word.
+        for(i = N_CMD_TOKENS - 1; i >= 1; i-- ){
+            if(commands[i] != NULL){
+                int is_last_ampersand = commands[i][strlen(commands[i])-1] == '&';
+                is_background_task = is_last_ampersand;
+                
+                if(is_background_task){
+                    // Remove ampersand. Make sure the command slot is null
+                    // if ampersand is last character, else just remove ampersand.
+                    if(strlen(commands[i]) == 1){
+                        commands[i] = NULL;
+                    }else{
+                        commands[i][strlen(commands[i])-1] = '\0';
+                    }
+                }
+                
+                // We only have to check the last non-null word if ampersand exists.
+                break;
+            }
         }
-
-        // for now just use first three
-        commands[0] = args_token[0];
-        commands[1] = args_token[1];
-        commands[2] = args_token[2];
-
-        // exit then just exit
-        if(my_strcmp(commands[0], "exit")){
-            should_run = 0;
+        
+        /*
+            Actual execution.
+        */
+        // Forking
+        pid_t pid;
+        int status;
+        pid = fork();
+        
+        if(pid < 0){
+            fprintf(stderr, "Failed to fork, pid: %d\n", pid);
+        }else if(pid == 0){ // if pid == 0, child process.
+            if(is_background_task){
+                setpgid(0,0); // make this child go background process
+            }
 
         // if not then execution mode
         }else{
@@ -134,23 +207,23 @@ int main(void) {
                 execvp(commands[0], commands);
             // if pid > 0, parent process.
             }else{
-                // if last arg is &, it == 0.
-                if(is_background_process){
-//                    printf("Parent process done: %s, %s, %s\n", args_token[0], args_token[1], args_token[2]);
-                }else{
-//                    printf("Parent process waiting: %s, %s, %s\n", args_token[0], args_token[1], args_token[2]);
-                    waitpid(pid, NULL, 0);
-//                    printf("Parent process done: %s, %s, %s\n", args_token[0], args_token[1], args_token[2]);
+                if(execvp(commands[0], commands) < 0){
+                    fprintf(stderr, "Unknown command: %s\n", commands[0]);
                 }
+            }
+            
+        }else{  // if pid > 0, parent process.
+            if(is_background_task){
+                // nothing...
+            }else{
+                waitpid(pid, NULL, 0);
+            }
 
-            }
-            // clean args
-            int j;
-            for(j = 0; j<HALF_LINE; j++){
-                args_token[j] = NULL;
-            }
         }
-
+        // free memory
+        free(args);
+        free(args_token);
+        free(commands);  
 
     }
 
